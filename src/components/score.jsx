4e5550585d2c660847c75cc13ef683c1c4077d98
Alen Abhilash <alenabhilash2@gmail.com>
2024-10-05 17:12:05 +0530

    Changed

--- src/components/score.jsx
+++ src/components/score.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 function ScoreBoard({score}) {
   return (
-    <div className='score-borded'>
-        <h2>{score}</h2>
+    <div className='score-borded p-12 bg-pink-400 w-25 h-25 rounded-2xl'>
+       <div className='w-30 h-12 bg-blue-500 p-3 rounded-xl'>
+       <h2 className='font-bold text-white  font-serif'> Score: {score}</h2>
+       </div>
     </div>
   )
 }
